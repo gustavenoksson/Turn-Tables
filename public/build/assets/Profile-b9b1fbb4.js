a81a68225799f1a918b649a856b597009d4fd33d
gustavenoksson <gustav.enoksson@live.se>
2023-11-22 12:26:47 +0100

    update new build

---
+++ public/build/assets/Profile-b9b1fbb4.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-6a7adc42.js";import"./Footer.style-09068167.js";function t(){return r.jsx("h1",{children:"Profile page"})}export{t as default};
--- public/build/assets/Profile-c0cdab45.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-7a5f6de1.js";import"./Footer.style-f0a59590.js";function t(){return r.jsx("h1",{children:"Profile page"})}export{t as default};
