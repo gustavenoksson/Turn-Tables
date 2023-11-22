f0d8c077245a47968ede943eb9959c640b9d777e
gustavenoksson <gustav.enoksson@live.se>
2023-11-22 15:32:41 +0100

    updated PreviousOrderCard component

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -1,4 +1,3 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { useEffect, useState } from 'react';
 import DeleteUserForm from './Partials/DeleteUserForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
@@ -8,11 +7,13 @@ import DefaultLayout from '@/Layouts/DefaultLayout';
 import styled from 'styled-components';
 import { Link } from '@inertiajs/react';
 import { fetchPreviousOrders } from '@/turn-table-studio/utils/sanity.queries';
+import PreviousOrderCard from '@/Components/PreviousOrderCard';
 
 const UpdateProfileArticle = styled.article`
     padding: 1rem;
     margin-bottom: 1.5rem;
     box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.06);
+    height: auto;
 `
 
 export default function Edit({ mustVerifyEmail, status, orders }) {
@@ -20,26 +21,27 @@ export default function Edit({ mustVerifyEmail, status, orders }) {
     const [previousOrders, setPreviousOrders] = useState([]);
     const [products, setProducts] = useState([]);
 
-    const groupedOrders = Object.groupBy(orders, ({ order_id }) => order_id);
-
-    const productIds = Array.from(
-        new Set(
-            orders.flatMap(order => order.product_id)
-        )
-    );
-
     useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const productIds = Array.from(
+                    new Set(
+                        orders.map(order => order.product_id)
+                    )
+                );
+    
+                const products = await fetchPreviousOrders(productIds);
+                setProducts(products);
 
-        fetchPreviousOrders()
-        .then((data) => {
-            setProducts(data);
-        })
-        .catch((error) => console.error('Error fetching products:', error));
-
-        setPreviousOrders(groupedOrders);
-    }, []);
-
-    console.log(previousOrders);
+                const groupedOrders = Object.groupBy(orders, ({ order_id }) => order_id);
+                setPreviousOrders(groupedOrders);
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
+    
+        fetchProducts();
+    }, [orders]);
 
     return (
         <DefaultLayout>
@@ -52,15 +54,11 @@ export default function Edit({ mustVerifyEmail, status, orders }) {
                     <UpdateProfileArticle>
                         <h2>Previous Orders</h2>
                         {Object.values(previousOrders).map(orderGroup => (
-                            <div key={orderGroup[0].order_id}>
-                                <h3>Order ID: {orderGroup[0].order_id}</h3>
-                                {orderGroup.map(individualOrder => (
-                                    <div key={individualOrder.id}>
-                                        <p>Product Id: {individualOrder.product_id}</p>
-                                        <p>Total Cost: {individualOrder.price} SEK</p>
-                                    </div>
-                                ))}
-                            </div>
+                            <PreviousOrderCard 
+                                key={orderGroup[0].order_id}
+                                previousOrders={orderGroup}
+                                products={products}>
+                            </PreviousOrderCard>
                         ))}
                     </UpdateProfileArticle>
 
